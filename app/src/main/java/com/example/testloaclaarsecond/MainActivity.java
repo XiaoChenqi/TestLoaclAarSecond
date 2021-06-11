2a18f2e5e6238b758687a749a5aa54a87db8a98b
xiaocq <516563076@qq.com>
2021-06-11 12:20:36 +0800

    添加了引用内网maven的方法

--- app/src/main/java/com/example/testloaclaarsecond/MainActivity.java
+++ app/src/main/java/com/example/testloaclaarsecond/MainActivity.java
@@ -1,17 +1,38 @@
 package com.example.testloaclaarsecond;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.blankj.utilcode.util.SizeUtils;
+import com.example.localone.DataUtils;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-import com.example.localone.Constant;
-
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mMethodTv;
+    String TAG="周杨，林晓旭，易霖";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initView();
+        mMethodTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               int xcq = DataUtils.getVoiceLineWight(10000);
+                Log.d(TAG, "onClick: "+xcq);
+
+            }
+        });
+
+    }
+
+    private void initView() {
+        mMethodTv = (TextView) findViewById(R.id.methodTv);
     }
 }
